fec2c3dee5b5eadecd31302e130f499194f5aea7
MasonVines <mason.vines@gmail.com>
2020-10-28 11:09:44 -0400

    Starting a draft seems to work and send everyone picks.

--- src/main/cube_bot/core.cljs
+++ src/main/cube_bot/core.cljs
@@ -1,22 +1,69 @@
 (ns cube-bot.core
   (:require ["discord.js" :as Discord]
             [clojure.string :as str]
-            [cube-bot.config :as config]))
+            [clojure.pprint :refer [pprint]]
+            [cube-bot.config :as config]
+            [cube-bot.cube :as cube]))
 
-
-(defonce *client (atom nil))
-
+(defn jsprint [js]
+  (.log js/console js))
 (defonce prefix "[]")
+(defonce *client (atom nil))
+(defonce *debug-message (atom nil))
 
-(defn handle-command [command]
-  (println command))
+;; {:cube [current cards]
+;;  :pack-number 1
+;;  :remaining-packs [[cards in pack]]
+;;  :seats [
+;;    {:player {:id user-id :picks [card picks]
+;;     :packs [[cards in pack] [cards in pack]]}}]} Packs are a Queue
+(defonce *draft (atom nil))
+
+(defn send-dm! [user-id text]
+  (-> (.. ^js @*client -users -cache)
+      (.get user-id)
+      (.send text)))
+
+(defn send-pack [seat]
+  (let [user-id (get-in seat [:player :id])
+        pack (first (:packs seat))
+        pack-string (str/join "\n" (map #(str %1 " - " %2) (range (count pack)) pack))]
+    (println pack-string)
+    (send-dm! user-id pack-string)))
+
+(defn build-seat [user-id pack]
+  {:player {:id user-id :picks []}
+   :packs [pack]})
+
+(defn start-draft [user-ids]
+  (println "Starting draft: " user-ids)
+  (let [packs (partition 15 cube/combo)
+        seats (map build-seat user-ids packs)]
+    (reset! *draft {:pack-number 1
+                    :remaining-packs (drop (count user-ids) packs)
+                    :seats seats})
+    (mapv send-pack (:seats @*draft))))
+
+(defn pick-card [userId cardNumber]
+  (println "Picking card: " userId " - " cardNumber))
+
+(defn handle-command [^js message]
+  (let [body (.-content message)
+        command-string (subs body (count prefix))
+        command-list (str/split command-string " ")
+        command (first command-list)
+        args (rest command-list)]
+    (condp = command
+      "newdraft" (start-draft (.. message -mentions -users keyArray))
+      "pick" (pick-card (.. message -author -id) (first args)))))
 
 ;; Handle messages
 (defn message-handler [^js message]
+  (reset! *debug-message message)
   (let [body (.-content message)]
     (when (and (not (.. message -author -bot))
              (str/starts-with? body prefix))
-      (handle-command (subs body (count prefix)))))
+      (handle-command message )))
 
   #_(if-not (= (.-author message) (.-user client))
     (cond
@@ -45,11 +92,11 @@
   (.login ^js @*client config/token))
 
 (defn ^:dev/after-load reload! []
-  (js/console.log "Code Reloaded")
+  (print "Code Reloaded")
   (.destroy ^js @*client)
   (connect))
 
 
 (defn -main []
-  (print "MAIN")
+  (print "Starting bot...")
   (connect))
